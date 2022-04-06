4a1b55209059ebc41befc0d635456944cbb7d2c0
BonggilB <95198463+BonggilB@users.noreply.github.com>
2022-04-06 18:10:43 +0900

    Add files via upload

---
+++ default.js
@@ -0,0 +1,44 @@
+let _btn=document.querySelectorAll('button');
+let _body=document.querySelector('body');
+let mainTit=document.querySelector('h1>a');
+let navList=document.querySelectorAll('li>a');
+let _li=document.querySelectorAll('h2');
+let _p=document.querySelectorAll('p');
+//마우스 오버 이벤트
+for(let idx=0;idx<_btn.length;idx++){
+    _btn[idx].addEventListener('mouseover',(event)=>{
+        event.target.style.opacity=1;
+    })
+}
+//마우스 아웃
+for(let idx=0;idx<_btn.length;idx++){
+    _btn[idx].addEventListener('mouseout',(event)=>{
+        event.target.style.opacity=0.3;
+    })
+}
+_btn[0].addEventListener('click',()=>{
+    _body.style.backgroundColor='#000';
+    mainTit.style.color='#f99';
+    for (let idx=0;idx<_li.length;idx++){
+        _li[idx].style.color='#aaf';
+    };
+    for (let idx=0;idx<navList.length;idx++){
+        navList[idx].style.color='#aaf';
+    };
+    for (let idx=0;idx<_p.length;idx++){
+        _p[idx].style.color='#aaf';
+    };
+})
+_btn[1].addEventListener('click',()=>{
+    _body.style.backgroundColor='#fff';
+    mainTit.style.color='#000';
+    for (let idx=0;idx<_li.length;idx++){
+        _li[idx].style.color='#000';
+    };
+    for (let idx=0;idx<navList.length;idx++){
+        navList[idx].style.color='#000';
+    };
+    for (let idx=0;idx<_p.length;idx++){
+        _p[idx].style.color='#000';
+    };
+})
